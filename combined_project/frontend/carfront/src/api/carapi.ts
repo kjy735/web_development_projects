af95d89c0b702ccd7ad205ba0e0f9ebcc049f9dd
kjy735 <wodus3806@gmail.com>
2025-04-11 03:12:47 +0900

    feat: 20250410 POST method finished

--- combined_project/frontend/carfront/src/api/carapi.ts
+++ combined_project/frontend/carfront/src/api/carapi.ts
@@ -1,4 +1,4 @@
-import { CarResponse } from "../types";
+import { CarResponse, Car } from "../types";
 import axios from "axios";
 
 export const getCars = async (): Promise<CarResponse[]> => {
@@ -6,3 +6,18 @@ export const getCars = async (): Promise<CarResponse[]> => {
 
     return response.data._embedded.cars;
   }
+
+  export const deleteCar = async (link: string): Promise<CarResponse> => {
+    const response = await axios.delete(link);
+    return response.data
+  }
+
+export const addCar = async (car: Car): Promise<CarResponse> => {
+  const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/cars`, car, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  return response.data;
+}
--- combined_project/frontend/carfront/src/components/Carlist.tsx
+++ combined_project/frontend/carfront/src/components/Carlist.tsx
@@ -1,13 +1,27 @@
-import { CarResponse } from "../types";
-import { useQuery } from "@tanstack/react-query";
-import { getCars } from "../api/carapi";
-import { DataGrid, GridColDef } from "@mui/x-data-grid"; // 데이터를 그리드 모양으로 나타내줌, 그리드의 컬럼명 지정
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getCars, deleteCar } from "../api/carapi";
+import { DataGrid, GridColDef, GridCellParams } from "@mui/x-data-grid"; // 데이터를 그리드 모양으로 나타내줌, 그리드의 컬럼명 지정
+import { Snackbar } from '@mui/material';
+import { useState } from "react";
+import AddCar from "./AddCar";
 
 function Carlist() {
+  const [open, setOpen] = useState(false);
+
+  const queryClient = useQueryClient();
 
   const { data, error, isSuccess } = useQuery({
     queryKey: ["cars"],
     queryFn: getCars
+  });
+
+  const { mutate } = useMutation(deleteCar, {
+    onSuccess: () => {
+      setOpen(true);
+      queryClient.invalidateQueries({queryKey: ['cars']});
+    }, onError: (err) => {
+          console.log(err);
+        },
   });
 
   const columns : GridColDef[] = [
@@ -17,6 +31,20 @@ function Carlist() {
     {field: 'registrationNumber', headerName: "Reg.nr", width: 150},
     {field: 'modelYear', headerName: "Model Year", width: 150},
     {field: 'price', headerName: "Price", width: 150},
+    {
+      field: 'edit',
+      headerName: '',
+      width: 90,
+      sortable: false,
+      filterable: false,
+      disableColumnMenu: true,
+      renderCell: (params: GridCellParams) =>
+        <button onClick={() => {
+          if(window.confirm(`${params.row.brand}의 ${params.row.model}을 삭제하시겠습니까?`)) {
+            mutate(params.row._links.car.href)
+          }
+        }}>삭제</button>
+    }
   ]
 
 
@@ -28,11 +56,24 @@ function Carlist() {
   }
   else {
     return (
-    <DataGrid 
-      rows={data}
-      columns={columns}
-      getRowId={row => row._links.self.href}
+      <>
+      <AddCar/>
+    
+      <DataGrid 
+        rows={data}
+        columns={columns}
+        getRowId={row => row._links.self.href}
+        // dispableRowSelectionOnClick={true}
       />
+
+      <Snackbar 
+        open={open}
+        autoHideDuration={2000}
+        onClose={() => setOpen(false)}
+        message="자동차가 삭제되었습니다."
+      />
+      </>
+
     );
   }
 }
